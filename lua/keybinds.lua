0b6472d7d3b88555612ffad784d7d4e3a3d46427
Mads Kristiansen <Mads@plikki.com>
2023-08-01 20:16:27 +0200

    refactor: changed keymaps from WH to keymap.set

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,31 +1,40 @@
 local wk = require("which-key")
 
+-- General Keybinds
+wk.register({
+    ["jk"] = { "<ESC>", "Escapes out of insert mode" },
+}, { mode = "i" })
+
+wk.register({
+    ["jk"] = { "<C-\\><C-n>", "Escapes out of insert mode" }
+}, { mode = "t" })
+
+
 -- Keybinds for debugging
 vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end)
 
 -- Keybinds for python debugging
 vim.keymap.set('n', '<leader>dpr', function() require('dap-python').test_method() end)
 
+
 -- Keybinds for git
 -- Git hunk selection
-wk.register({
-    ["<leader>h"]   =   { name = "GitHunk"},
-    ["<leader>hn"]  =   {":Gitsigns next_hunk<CR>","Next hunk of git code"},
-    ["<leader>hN"]  =   {":Gitsigns prev_hunk<CR>","Previous hunk of git code"},
-    ["<leader>hp"]  =   {":Gitsigns preview_hunk<CR>","Show current hunk"},
-    ["<leader>hs"]  =   {":Gitsigns stage_hunk<CR>","Stage current hunk"},
-    ["<leader>hr"]  =   {":Gitsigns reset_hunk<CR>","Resets current hunk"},
-    ["<leader>hc"]  =   {":Git commit | startinsert<CR>","Commits what is in buffer"},
-})
+wk.register({ ["<leader>h"] = { name = "+Git" }, })
+vim.keymap.set("n", "<leader>hn", ":Gitsigns next_hunk<CR>", { desc = "Next hunk of git code" })
+vim.keymap.set("n", "<leader>hN", ":Gitsigns prev_hunk<CR>", { desc = "Previous hunk of git code" })
+vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk<CR>", { desc = "Show current hunk" })
+vim.keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage current hunk" })
+vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Resets current hunk" })
+vim.keymap.set("n", "<leader>hc", ":Git commit | startinsert<CR>", { desc = "Commits what is in buffer" })
 
 -- Telescope edits
-wk.register({
-    ["<leader>f"]     =   {name = "+file"},
-    ["<leader>ff"]  =   {"<cmd>Telescope find_files<cr>", "Fuzzy find File"},
-    ["<leader>fr"]  =   {"<cmd>Telescope oldfiles<cr>", "Fuzzy find Recent File"},
-    ["<leader>fl"]  =   {"<cmd>Telescope live_grep<cr>", "fuzzy find with grep"},
-    ["<leader>fb"]  =   {"<cmd>Telescope buffers<cr>", "Fuzzy find open File"},
-    ["<leader>fg"]  =   {"<cmd>Telescope git_commits<cr>", "Fuzzy find git commits"},
-})
+wk.register({["<leader>f"] = { name = "+File" },})
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find File" })
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find Recent File" })
+vim.keymap.set("n", "<leader>fl", "<cmd>Telescope live_grep<CR>", { desc = "fuzzy find with grep" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Fuzzy find open File" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope git_commits<CR>", { desc = "Fuzzy find git commits" })
 
-vim.cmd('map <Leader>lf :lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>')
+-- LSP Keybinds
+wk.register({["<leader>l"] = { name = "+LSP" },})
+vim.keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.format({ timeout_ms = 2000})<CR>", { desc = "Format document [LSP]" })
