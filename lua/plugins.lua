15ec7f7ff0f47c9950ffb209ad442bdf35e0e91b
Mads Kristiansen <Mads@plikki.com>
2023-08-02 08:36:08 +0200

    fix: remove unused option in config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,7 @@ return {
             "mfussenegger/nvim-dap",
             "rcarriga/nvim-dap-ui",
         },
-        config = function(_,opts)
+        config = function()
             local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
             require("dap-python").setup(path)
         end,
