114729c72a0017a54918953b5230128eb9c5c862
Mads Kristiansen <mads@plikki.com>
2024-02-23 13:21:06 +0100

    Lazy start

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,18 +1,12 @@
 return {
-     'rose-pine/neovim', name = 'rose-pine',
-        priority = 1000,
-        config = function()
-            require('rose-pine').setup({
-                --- @usage 'auto'|'main'|'moon'|'dawn'
-                variant = 'auto',
-                --- @usage 'main'|'moon'|'dawn'
-                dark_variant = 'main',
-                bold_vert_split = false,
-                dim_nc_background = false,
-                disable_background = true,
-                disable_float_background = false,
-                disable_italics = false,
-            })
-            vim.cmd([[colorscheme rose-pine]])
-    end,
+  "rose-pine/neovim",
+  name = "rose-pine",
+  priority = 1000,
+  config = function()
+    require("rose-pine").setup({
+      variant = "moon",
+      dark_variant = "main",
+      disable_background = "true",
+    })
+  end,
 }
